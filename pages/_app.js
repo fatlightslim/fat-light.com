84875050c5cef54305d8718e0cc452578c429955
abiru <masataka.abiru@gmail.com>
2021-03-24 15:35:26 +0900

    add tagmanager

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,14 @@ import { useRouter } from "next/router"
 import { useState, useEffect } from "react"
 import { CartProvider } from "react-use-cart"
 import * as data from "../manifest.json"
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+    gtmId: 'G-6BFW05MB41'
+}
+
+TagManager.initialize(tagManagerArgs)
+
 const products = data.default
 
 const MyApp = ({ Component, pageProps }) => {
